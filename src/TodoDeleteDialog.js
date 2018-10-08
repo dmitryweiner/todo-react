de1d1daf24c9a767fb34baa20c2948f4f71e3f45
Dmitry Weiner <dmitry.weiner@gmail.com>
2018-10-08 20:26:53 +0700

    Improve delete dialog

--- src/TodoDeleteDialog.js
+++ src/TodoDeleteDialog.js
@@ -1,17 +1,25 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 class TodoDeleteDialog extends Component {
 
+    handleDeleteButton() {
+        console.log('delete id = ', this.props.match.params.id);
+    }
+
     render() {
-        return <div>
-            <h4>Delete?</h4>
-        </div>
+        return <form>
+            <fieldset>
+                <legend>Delete item?</legend>
+                <div className="col-sm-10">
+                    <Link className="btn btn-info" to="/">Cancel</Link>
+                    &nbsp;
+                    <Link className="btn btn-success" to="/" onClick={() => this.handleDeleteButton()}>Delete</Link>
+                </div>
+            </fieldset>
+        </form>
     }
 }
-
-TodoDeleteDialog.propTypes = {
-    id: PropTypes.number.isRequired
-};
 
 export default TodoDeleteDialog;
